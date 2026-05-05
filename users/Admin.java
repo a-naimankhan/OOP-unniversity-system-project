77046e531a1d1024a14a597c9f083d50f1b5ebfa
Alexander Demchenko <alexandersd3306@gmail.com>
2026-05-05 14:32:22 +0500

    simplify user addition and removal logic in Admin class

--- users/Admin.java
+++ users/Admin.java
@@ -1,70 +1,68 @@
 package users;
 
 import java.io.Serializable;
+import java.util.List;
+import java.util.Vector;
 
-public class Admin extends Employee implements Serializable, Comparable<Object>{
-	/**
-	 * 
-	 */
+public class Admin extends Employee implements Serializable {
+
 	private static final long serialVersionUID = 1L;
-	public Admin() {
-		
-	}
+
+	public Admin() {}
+
 	public Admin(String fullName, String username, String password, int salary) {
 		super(fullName, username, password, salary);
 	}
-	
-	/**
-     * @param user 
-	 * @return 
-     */
+
 	public void addUser(User user) {
 		Database.users.add(user);
 		if (user instanceof Student) {
-			Student s = (Student)user;
-			Database.students.add(s);
+			Database.students.add((Student) user);
+		} else if (user instanceof Teacher) {
+			Database.teachers.add((Teacher) user);
+		} else if (user instanceof Manager) {
+			Database.managers.add((Manager) user);
+		} else if (user instanceof Admin) {
+			Database.admins.add((Admin) user);
+		} else if (user instanceof Employee) {
+			Database.employees.add((Employee) user);
 		}
-		else if (user instanceof Teacher) {
-			Teacher t = (Teacher)user;
-			Database.teachers.add(t);
-		}
-		else if (user instanceof Manager) {
-			Manager m = (Manager)user;
-			Database.managers.add(m);
-		}
-		else if (user instanceof Admin) {
-			Admin a = (Admin)user;
-			Database.admins.add(a);
-		}
-		else if (user instanceof Employee) {
-			Employee e = (Employee)user;
-			Database.employees.add(e);
-		}
+		Database.log("ADD_USER: " + user.getClass().getSimpleName() + " '" + user.getUsername() + "'");
 	}
+
 	public void removeUser(User user) {
 		Database.users.remove(user);
 		if (user instanceof Student) {
-			Student s = (Student)user;
-			Database.students.remove(s);
+			Database.students.remove((Student) user);
+		} else if (user instanceof Teacher) {
+			Database.teachers.remove((Teacher) user);
+		} else if (user instanceof Manager) {
+			Database.managers.remove((Manager) user);
+		} else if (user instanceof Admin) {
+			Database.admins.remove((Admin) user);
+		} else if (user instanceof Employee) {
+			Database.employees.remove((Employee) user);
 		}
-		else if (user instanceof Teacher) {
-			Teacher t = (Teacher)user;
-			Database.teachers.remove(t);
-		}
-		else if (user instanceof Manager) {
-			Manager m = (Manager)user;
-			Database.managers.remove(m);
-		}
-		else if (user instanceof Admin) {
-			Admin a = (Admin)user;
-			Database.admins.remove(a);
-		}
-		else if (user instanceof Employee) {
-			Employee e = (Employee)user;
-			Database.employees.remove(e);
-		}
+		Database.log("REMOVE_USER: " + user.getClass().getSimpleName() + " '" + user.getUsername() + "'");
 	}
-	public void updateUser(User user) {
-		Database.users.add(user);
+
+	public void updateUser(User oldUser, User newUser) {
+		int idx = Database.users.indexOf(oldUser);
+		if (idx >= 0) Database.users.set(idx, newUser);
+		Database.log("UPDATE_USER: '" + oldUser.getUsername() + "' → '" + newUser.getUsername() + "'");
+	}
+
+	public List<String> viewLogs() {
+		return Database.logs;
+	}
+
+	public void clearLogs() {
+		Database.logs.clear();
+		Database.log("LOGS_CLEARED by " + this.getUsername());
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + " Admin";
 	}
 }
